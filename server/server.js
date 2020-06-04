fd6baf57d236a9d015ac031fbb65925eb019789e
Eric Wang <matchstik@MacBook-Pro.local>
2020-06-04 09:20:00 -0700

    basic server Hello World running

--- server/server.js
+++ server/server.js
@@ -1,17 +1,12 @@
   
 const express = require('express');
-const path = require('path');
-// const db = require('../database/index.js');
 
 const app = express();
 const port = 8000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+app.get('/', function(req, res){
+  res.send("Hello World!");
 });
-
-app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);

056fbc00c9b3c8c3c2a189a51557c2beb3392f52
Eric Wang <matchstik@MacBook-Pro.local>
2020-06-04 08:25:49 -0700

    basic server setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+  
+const express = require('express');
+const path = require('path');
+const db = require('../database/index.js');
+
+const app = express();
+const port = 3001;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
